faa920c9937a17571b4bd223687bdb54f1501ce4
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2025-04-04 21:46:27 -0300

    resolvido

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿double comprimentoescada, anguloeradiano, anguloemgraus, altura;
+
+   Console.Write("informe o comprimento da escada");
+   comprimentoescada = Convert.ToDouble(Console.ReadLine());
+
+   Console.Write("Informe o angulo da escada em graus:   ");
+   anguloemgraus = Convert.ToDouble(Console.ReadLine());
+
+   anguloeradiano = anguloemgraus * (Math.PI / 180);
+
+   altura = comprimentoescada * Math.Sin(anguloeradiano);
+
+   Console.WriteLine($"{altura:N2}");
